baa71ed1816ceeb51febcc8140ccb94130b7f484
Francesca Giordano <fg6@sanger.ac.uk>
2017-03-27 16:47:12 +0100

    Gaussian shape option with possibility to suppress original peak

--- subrand.py
+++ subrand.py
@@ -23,15 +23,28 @@ warning=0.6 # about min ratio of coverage needed/initial coverage (avg prob for 
 compall=[]
 
 def read(inputfile,faqtype,cov,refsize):
-  records = (r for r in SeqIO.parse(inputfile, faqtype)) 
+
   global totall
   global readall
   global meantot
   global inin
+  global randord
+  global lrecords
 
   global totN
   global weigth
 
+
+  lrecords = list(SeqIO.parse(inputfile, faqtype))
+
+  listord=range(0,len(lrecords))
+  nn=len(listord)
+  for ir in range(0,nn):
+    thisr=random.choice(listord)  
+    randord.append(thisr)
+    listord.remove(thisr)
+
+ 
 
   global warning
   if mean is 0:
@@ -42,12 +55,13 @@ def read(inputfile,faqtype,cov,refsize):
   weigth=1.
   totN=0
   loctot=0
-  for r in records:
-     totall+=len(r.seq) 
-     readall.append(len(r.seq))
-     if mean > 0 and len(r.seq)< mean+2*std and len(r.seq)> mean-2*std: 
-        meantot+= len(r.seq)
-        loctot+= 1
+
+  for ir in range(0,len(lrecords)):
+     totall+=len(lrecords[ir].seq)
+     readall.append(len(lrecords[ir].seq))
+     if mean > 0 and len(lrecords[ir].seq)< mean+2*std and len(lrecords[ir].seq)> mean-2*std: 
+        meantot+= len(lrecords[ir].seq)
+        loctot+= 1     
      totN+=1
 
   if mean == 0:
@@ -96,21 +110,22 @@ def plotnow():
         'size'   : 10}
     plt.rc('font', **font)
      
+    binwidth=400
     fig = plt.figure()
-    plt.hist(readall,100,histtype='step',color='b',label="Initial reads")
-    values, bins, _ = plt.hist(subreads,100,histtype='step',color='r',label="Sub-selected reads")
+    plt.hist(readall, bins=np.arange(0, max(readall) + binwidth, binwidth),histtype='step',color='b',label="Initial reads")
+    values, bins, _ = plt.hist(subreads,bins=np.arange(0, max(subreads) + binwidth, binwidth),histtype='step',color='r',label="Sub-selected reads")
     if len(compall) > 0:
-      plt.hist(compall,100,histtype='step',color='g',label="Comparison reads")
+      plt.hist(compall,bins=np.arange(0, max(compall) + binwidth, binwidth),histtype='step',color='g',label="Comparison reads")
 
 
     plt.legend(bbox_to_anchor=(0, 0, 1, 1), loc="upper right", borderaxespad=0.,fontsize=10, title='Read length distribution')
 
 
     # plot gaussian
-    if(mean is not 0):
-      sub_area = sum(np.diff(bins)*values)
-      x = np.linspace(0, max(readall), 100)
-      plt.plot(x,norm.pdf(x, mean, std)*sub_area,color='r')
+    #if(mean is not 0):
+    #  sub_area = sum(np.diff(bins)*values)
+    #  x = np.linspace(0, max(readall), 100)
+    #  plt.plot(x,norm.pdf(x, mean, std)*sub_area,color='r')
       
     plt.ylabel('N')
     plt.xlabel('Read lengths')
@@ -123,18 +138,31 @@ def plotnow():
     myfig.close()
 
 def gaussian(x, mu, sig):
-    return 1.*np.exp(-np.power((x - mu)/sig, 2.)/2)
+   n=1./np.sqrt(2*np.pi*sig*sig)
+   exp=(x - mu)*1./sig
+   return n*np.exp(-exp*exp/2)
 
 
 def readnrandom(inputfile,faqtype,ofaqtype,cov,refsize): 
 
   global subreads
-
-
-  records = list(SeqIO.parse(inputfile, faqtype))
   readlist=[]
+
+
+  if mean>0:
+    xx = np.linspace(0, max(readall), 100)
+    vv=[gaussian(x,mean,std) for x in xx]
+    gmax=max(vv)   
+    #mean2=1000
+    #std2=4000
+    #vv2=[gaussian(x,mean2,std2) for x in xx]
+    #gmax2=max(vv2)   
+    #mo=1350
+    #stdo=5000
+    vv3=[gaussian(x,omean,ostd) for x in xx]
+    gmax3=max(vv3)   
+
  
-  
   if(mean==0):
     mytotal=totall
   else:
@@ -145,19 +173,9 @@ def readnrandom(inputfile,faqtype,ofaqtype,cov,refsize):
   i=-1
   ii=0
 
-  # choose a random, non-repetitive order to go through reads:
-  randord=[]
-  listord=range(0,len(records))
-  nn=len(listord)
-  for ir in range(0,nn):
-    thisr=random.choice(listord)  
-    randord.append(thisr)
-    listord.remove(thisr)
-
-
   # go through reads and subselect them
   for ir in randord:
-   thislen=len(records[ir].seq)
+   thislen=len(lrecords[ir].seq)
    allmean+=thislen
    i+=1
 
@@ -172,16 +190,24 @@ def readnrandom(inputfile,faqtype,ofaqtype,cov,refsize):
       ran=random.randint(0,100)
       if ran > value: chosen=1
     else:
-      weigth=1
-      if(thislen<3000): weigth=0.8
-      model=gaussian(thislen,mean,std)
-      sigma_up=model*weigth  
+      # does not work well... 
+      #weigth=1
+      #if(thislen<3000): weigth=0.8
+      #model1=(gaussian(thislen,mean,std)/gmax) #*weigth
+            
+      model1=1*gaussian(thislen,mean,std)/gmax
+      if model1>1: model1=1.
+      model2=0
+      model3=corrfact*gaussian(thislen,omean,ostd)/gmax3
+
+
+      model=model1+model2-model3
       ran=random.uniform(0,1)
 
-      if ran <= sigma_up: chosen=1
+      if ran <= model: chosen=1
 
     if chosen: ## add to selected reads  
-     readlist.append(records[ir])
+     readlist.append(lrecords[ir])
      mysum+=thislen   
      subreads.append(thislen) 
      ii+=1
@@ -196,12 +222,12 @@ def readnrandom(inputfile,faqtype,ofaqtype,cov,refsize):
     print (" Final number of bases=%0.0f,  Coverage=%0.0fX" % (mysum,float(mysum)/refsize))
     print (" Initial read mean-length: %0.1f  Final read mean-length: %0.1f" %(allmean/i,mysum/ii))
 
-  ofile="subreads_%0.0fX.%s" % (cov,ofaqtype)
-  if mean > 0: ofile="subreads_%0.0fX_mean%d.%s" % (cov,mysum/ii,ofaqtype)
-  output_handle = open(ofile,"w")  
-  SeqIO.write(readlist, output_handle,ofaqtype )
-  output_handle.close()
-  print (" Subsample written in",ofile,", read length distribution plotted in lengths.pdf\n")
+  #ofile="subreads_%0.0fX.%s" % (cov,ofaqtype)
+  #if mean > 0: ofile="subreads_%0.0fX_mean%d.%s" % (cov,mysum/ii,ofaqtype)
+  #output_handle = open(ofile,"w")  
+  #SeqIO.write(readlist, output_handle,ofaqtype )
+  #output_handle.close()
+  #print (" Subsample written in",ofile,", read length distribution plotted in lengths.pdf\n")
 
 
 def usage():
@@ -223,9 +249,16 @@ def main(argv):
    refsize = 0
    global mean
    global std
+   global omean
+   global ostd
+   global randord
+   global corrfact
+
+   randord=[]
+
 
    try:
-      (opts, args) = getopt.getopt(argv,"i:t:c:r:m:s:p:",["ifile=","faqtype=","cov=","refsize=","mean=","std=","compfile="])   
+      (opts, args) = getopt.getopt(argv,"i:t:c:r:m:s:p:o:u:f:",["ifile=","faqtype=","cov=","refsize=","mean=","std=","compfile=","origmean","origstd","corrfactor"])   
    except getopt.GetoptError:
       usage()
       sys.exit(2)
@@ -247,7 +280,13 @@ def main(argv):
        mean = float(arg)
      elif opt in ("-s", "--std"):
        std = float(arg)
-         
+     elif opt in ("-o", "--omean"):
+       omean = float(arg)
+     elif opt in ("-u", "--ostd"):
+       ostd = float(arg)
+     elif opt in ("-f", "--corrfac"):
+       corrfact = float(arg)
+          
    print(compfile)
    if len(inputfile) == 0 or cov ==0 or refsize ==0:
       usage()
@@ -269,6 +308,9 @@ def main(argv):
    print ("\n Getting a", "{:.0f}".format(cov),"X subsample of reads from", inputfile)
    print ("   (Coverage calculated for a reference size of %0.1f Mb)" % (refsize/1000000))
   
+
+  # print(mean,omean,ostd,corrfact)
+   
    
    # read file and check if enough data for desired subsample
    ok=read(inputfile,faqtype,cov,refsize)
